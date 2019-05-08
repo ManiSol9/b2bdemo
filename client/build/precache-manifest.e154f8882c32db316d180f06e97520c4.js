b3acd7c2c7c313448a1f754b73d02081e142c578
Manikanta Pedireddy <pedireddyvishnu.chakramanikanta@wipro.com>
2019-05-08 18:57:05 +0530

    latest changes

---
+++ client/build/precache-manifest.e154f8882c32db316d180f06e97520c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66993d8fa2f95e1df34ffd200298706a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0c45531df6c372e24b0",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "470ee7f31cab2cf1dd4e",
+    "url": "/static/css/main.85bdcab4.chunk.css"
+  },
+  {
+    "revision": "e0c45531df6c372e24b0",
+    "url": "/static/js/2.886cd0c1.chunk.js"
+  },
+  {
+    "revision": "470ee7f31cab2cf1dd4e",
+    "url": "/static/js/main.23d0948a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
